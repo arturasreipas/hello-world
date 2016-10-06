c3061680c895dcd81080edb8ed51bc998d22ecd8
arturas <arturas.reipas2@gmail.com>
2016-10-06 14:53:00 +0300

    Add Hello module to application

--- hello-world/Program.cs
+++ hello-world/Program.cs
@@ -6,13 +6,15 @@ namespace hello_world
     {
         private static void Main(string[] args)
         {
-            if(args.Length < 1)
+            if (args.Length < 1)
             {
                 Console.WriteLine("Program requiress at least one argument");
                 return;
             }
 
-            Console.WriteLine(string.Format("Hello {0}!", args[0]));
+            var module = new HelloModule();
+
+            Console.WriteLine(module.SayHello(args[0]));
 
             Console.ReadLine();
         }
